4fa684cfaaf39043995ba9fc297157fd84e01167
dcartist <pbannerman13@gmail.com>
2021-09-18 11:55:37 -0400

    update columns

--- js/script.js
+++ js/script.js
@@ -4,24 +4,28 @@ function sushiMaker (amount){
 
     for (let i = 0; i < amount; i++){
         let plate = document.querySelector(".inside")
-        let sushiDiv = document.createElement('div')
+        // let sushiDiv = document.createElement('div')
         let sushi = document.createElement('img')
-        if (i < 5){
-            sushi.src=`images/sushi/sushi-0${count}_resize.png`
+        if (i < 5 & i > -1){
+            sushi.src=`images/sushi/sushi-03_resize.png`
+            sushi.classList = "sushi"
+            plate.appendChild(sushi)
+        } else if (i > 4 & i < 10){
+            sushi.src=`images/sushi/sushi-04_resize.png`
+            sushi.classList = "sushi"
+            plate.appendChild(sushi)
         } else {
-            sushi.src=`images/sushi/sushi-0${count + 1}_resize.png`
+            sushi.src=`images/sushi/sushi-0${count + 2}_resize.png`
+            sushi.classList = "sushi"
+            plate.appendChild(sushi)
         }
        
-        sushi.classList = "sushi"
-        sushiDiv.classList = "sushiDiv"
-    
-        // sushiDiv.appendChild(sushi);
-        plate.appendChild(sushi)
+        
     }
    
 }
 
-sushiMaker(10)
+sushiMaker(15)
 
 let switchToggle = document.querySelector('#switch');
 switchToggle.onchange = function(){
@@ -36,4 +40,18 @@ switchToggle.onchange = function(){
         console.log(switchToggle.classList)
         console.log(switchToggle)
     }
+}
+
+
+let gridColumns = document.querySelector("#gridColumns")
+
+gridColumns.onchange = function(e){
+    let plateColumns = document.querySelector(".grid-selection")
+    e.preventDefault()
+    let text = "1fr ".repeat(e.target.value)
+    // let text = Array(e.target.value).join("a")
+
+    console.log(text)
+    console.log(e.target.value)
+    plateColumns.style.gridTemplateColumns=`repeat(${e.target.value}, 1fr)`
 }
