6a64908559dbc52712f70eb18532c3858f181358
dcartist <pbannerman13@gmail.com>
2021-10-02 17:57:01 -0400

    removing wood texture

--- js/script.js
+++ js/script.js
@@ -9,7 +9,6 @@ function sushiMaker(amount) {
 
     for (let i = 0; i < amount; i++) {
         let plate = document.querySelector(".inside")
-        // let sushiDiv = document.createElement('div')
         let sushi = document.createElement('img')
         if (i < 5 & i > -1) {
             sushi.src = `images/sushi/sushi-03_resize.png`
